bed1fddc8692adb8ffae477edd36461fbff36418
DanielKwakye <dannykay.developer@gmail.com>
2025-02-06 14:47:50 -0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+  // const [count, setCount] = useState(0)
+
+  return (
+      <div className="container mx-auto mt-10 space-y-4">
+          <h1 className="text-3xl">
+              Welcome to Listing and Property Management System
+          </h1>
+          <div>
+              <Button>Click me</Button>
+          </div>
+      </div>
+  )
+}
+
+export default App
