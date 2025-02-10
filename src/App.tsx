055670676684b1aecdeeccd18b599bbad88d9de5
DanielKwakye <dannykay.developer@gmail.com>
2025-02-10 14:54:41 -0330

    merged feature/navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import {Button} from "@/components/ui/button.tsx";
-import Navbar from "./components/website/Navbar";
 
 function App() {
 
   // const [count, setCount] = useState(0)
 
   return (
-    //   <div className="container mx-auto mt-10 space-y-4">
-    //       <h1 className="text-3xl">
-    //           Welcome to Listing and Property Management System
-    //       </h1>
-    //       <div>
-    //           <Button>Click me</Button>
-    //       </div>
-    //   </div>
-      <div>
-        <Navbar />
+      <div className="container mx-auto mt-10 space-y-4">
+          <h1 className="text-3xl">
+              Welcome to Listing and Property Management System
+          </h1>
+          <div>
+              <Button>Click me</Button>
+          </div>
       </div>
+
   )
 }
 
