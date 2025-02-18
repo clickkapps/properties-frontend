c1e9578a50bde8875a052d2843a150be598cba2a
DanielKwakye <dannykay.developer@gmail.com>
2025-02-18 05:54:28 -0330

    merged sign in into main

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
-<<<<<<< HEAD
 import LandingPage from "@/presentation/website/LandingPage.tsx";
-// import Packages from "./components/website/Packages";
-=======
-import {Button} from "@/components/ui/button.tsx";
-import SignIn from "./components/website/SignIn";
->>>>>>> feature/sign-in
+
 
 function App() {
 
   // const [count, setCount] = useState(0)
 
   return (
-<<<<<<< HEAD
       <div >
           <LandingPage />
       </div>
-=======
-    //   <div className="container mx-auto mt-10 space-y-4">
-    //       <h1 className="text-3xl">
-    //           Welcome to Listing and Property Management System
-    //       </h1>
-    //       <div>
-    //           <Button>Click me</Button>
-    //       </div>
-    //   </div>
-
-    <SignIn />
->>>>>>> feature/sign-in
   )
 }
 
