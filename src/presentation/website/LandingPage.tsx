9624b4202dca9c0480c067efe67ccfc6413d183c
DanielKwakye <dannykay.developer@gmail.com>
2025-02-19 05:03:18 -0330

    worked on the spacing between the heading the listing

--- src/presentation/website/LandingPage.tsx
+++ src/presentation/website/LandingPage.tsx
@@ -12,14 +12,21 @@ function LandingPage() {
             {/*------- End of Header ------------*/}
 
 
-            <div className="-mt-[0px] md:-mt-[0px] container mx-auto">
+            <div className="container mx-auto -mt-[20vh]">
+
+                {/* Because of the featured listing height, we need to give space below to accomodate it*/}
+                <div className="h-[100px]"></div>
+
+                {/* This is the margin that equals the title space below*/}
+                <div className="h-[50px]"></div>
 
                 {/*------- Exclusive Listings ------------*/}
                 <section id="listings">
                     <h2 className="mx-12 text-3xl font-bold text-center"> Exclusive Listings </h2>
-                    <p className="text-center my-2 mx-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
+                    <p className="text-center my-2 mx-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Culpa
                         dicta ex itaque neque officia repellat sunt. Inventore similique totam voluptates?</p>
-                    <div className="h-[52px]"></div>
+                    <div className="h-[50px]"></div>
                     {/*<div className="grid grid-cols-5">*/}
 
 
