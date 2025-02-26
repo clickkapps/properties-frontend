36fabfe3f0c8c1f64ff56a5e51c3997617a749a5
DanielKwakye <dannykay.developer@gmail.com>
2025-02-26 15:48:54 -0330

    commented out unused import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import LandingPage from "@/presentation/website/LandingPage.tsx";
 import {BrowserRouter, Route, Routes} from "react-router";
 import SignIn from "@/components/website/SignIn.tsx";
 import ScrollToTop from "@/lib/custom-hooks/ScrollToTop.ts";
-import AgentCard from "./components/website/AgentCard";
+// import AgentCard from "./components/website/AgentCard";
 import AgentInfoCard from "./components/website/AgentInfoCard";
 
 
@@ -16,7 +16,7 @@ function App() {
               <Route path="/" element={<LandingPage />} />
               <Route path="/login" element={<SignIn />} />
               <Route path="/signup" element={<SignIn />} />
-              <Route path="agentcard" element={<AgentInfoCard />} />
+              <Route path="/agentcard" element={<AgentInfoCard />} />
           </Routes>
       </BrowserRouter>
   )
