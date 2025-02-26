178f43d27a6d9cdd80b3c84f6fab0f3eb9ba8183
DanielKwakye <dannykay.developer@gmail.com>
2025-02-26 15:55:14 -0330

    removed routes to search form

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import LandingPage from "@/presentation/website/LandingPage.tsx";
 import {BrowserRouter, Route, Routes} from "react-router";
 import SignIn from "@/components/website/SignIn.tsx";
 import ScrollToTop from "@/lib/custom-hooks/ScrollToTop.ts";
-import SearchResults from "./components/website/SearchResults";
-import MobileSearchBar from "./components/website/MobileSearchBar";
 
 
 function App() {
@@ -16,8 +14,6 @@ function App() {
               <Route path="/" element={<LandingPage />} />
               <Route path="/login" element={<SignIn />} />
               <Route path="/signup" element={<SignIn />} />
-              <Route path="mobilesearch" element={<MobileSearchBar />} />
-              <Route path="searchresults" element={<SearchResults />} />
           </Routes>
       </BrowserRouter>
   )
