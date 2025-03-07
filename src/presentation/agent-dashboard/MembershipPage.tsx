a9d5c3f79f9dda3dc10214ab2bd4ddbc8fdd1fd6
elton asamoah <eltonasamoah007@gmail.com>
2025-03-07 08:02:36 +0000

    Reworked on membership card

--- src/presentation/agent-dashboard/MembershipPage.tsx
+++ src/presentation/agent-dashboard/MembershipPage.tsx
@@ -1,16 +1,19 @@
 import MembershipCard from "@/components/agent-dashboard/MembershipCard"
-import Packages from "@/components/website/Packages"
+import MembershipPackages from "@/components/agent-dashboard/MembershipPackages"
 
 function MembershipPage() {
     return (
-        <div className="container mx-auto  ">
-            <h2 className="text-2xl font-semibold mb-8">Membership</h2>
+        <div className="container mx-auto h-screen ">
 
-            <div>
-            <MembershipCard />
-            <Packages />
-            </div>
+            <h2 className="text-2xl font-semibold mb-8">Membership</h2>
             
+            <div className="mb-6">
+            <MembershipCard />
+            </div>
+            <div className="">
+            <MembershipPackages />
+            </div>
+
         </div>
     )
 }
